0319ba008d98b469957bb6a079620ba671a8e543
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-09-23 03:13:03 -0400

    Create Hero data object.

--- client/src/pages/HomePage/HomePage.data.js
+++ client/src/pages/HomePage/HomePage.data.js
@@ -2,6 +2,7 @@ export const heroObject = {
 	img         : require('../../assets/online_connection.svg'),
 	src         : 'Hero image',
 	header      : 'Everything your LMS should be and more.',
-	description : 'Manage, track, and achieve your learning goals with Outfox LMS. ',
+	description :
+		'Manage, track, and achieve your learning goals with Outfox LMS. Group and share resources with friends. No cost, no expiration, a free lifetime of learning.',
 	buttonLabel : 'Get Started'
 };
