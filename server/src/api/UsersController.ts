1b79bcf37531f896c5b949c6b3f39b55ff92256a
John Gustafson <jgustafson1997@hotmail.com>
2020-10-01 17:29:57 -0400

    Get a single user by id works

--- server/src/api/UsersController.ts
+++ server/src/api/UsersController.ts
@@ -10,7 +10,7 @@ class UsersController {
     // Path that is required in order to access the api http://localhost:8080/routes/api/users
     public path = '/api/users';
     public router = express.Router();
-   
+
     constructor() {
         this.initializeRoutes();
     }
@@ -20,48 +20,63 @@ class UsersController {
      * Ex. GET, PUT, POST, UPDATE, etc
      */
     public initializeRoutes() {
-        
         this.router.route(this.path)
             .get(this.getAllUsers)
             .post(this.createAUser);
-        //this.router.route(this.path + '/:id')
-            //.get(this.getAUser);
+        this.router.route(this.path + '/:id')
+            .get(this.getAUser);
     }
 
-    /*
-    getAUser = (request: express.Request, response: express.Response) => {
-        const found = users.some(user => user.userID === parseInt(request.params.id, 10));
-        if (found) {
-            response.json(users.filter(user => user.userID === parseInt(request.params.id, 10)));
-        } else {
-            response.status(400).json({message: `User with id ${request.params.id} not found`});    
-        } 
-    }*/
+    /**
+     * Grabs a specific user based off the ID provided
+     * @param request HTTP browser request
+     * @param response HTTP browser response
+     */
+    getAUser = async (request: express.Request, response: express.Response) => {
+        try {
+            const { id } = request.params;
+            const user = await User.findOne({
+                where: {userid: id},
+            });
 
-     getAllUsers = async (request: express.Request, response: express.Response) => {
-            const user = await User.findAll().catch((err) => {
-            response.status(400);
-        });
-        
-        if (user != null) {
-            response.json(user);
-        } else {
-            response.status(400).json({message: 'No registered users'});
+            if (user) {
+                response.status(200).json(user);
+            } else {
+                response.status(404).send('User with the specified ID does not exist');
+            }
+        } catch (err) {
+            response.status(500).send(err.message);
         }
     }
 
+    /**
+     * Grabs all users in the database and sends them as a response in json
+     * @param request HTTP browser request
+     * @param response HTTP browser response
+     */
+    getAllUsers = async (request: express.Request, response: express.Response) => {
+        try {
+            const user = await User.findAll();
+            response.json(user);
+        } catch (err) {
+            response.status(400).json({ message: 'No registered users' });
+        }
+    }
+
+    /**
+     * Creates a new user in the database if the request has the correct json
+     * @param request HTTP browser request
+     * @param response HTTP browser response
+     */
     createAUser = async (request: express.Request, response: express.Response) => {
         try {
             // If missing non-nullable fields it will create an error
             const user = await User.create(request.body);
-            response.status(201).json({
-                user,
-            });
+            response.status(201).json({ user });
         } catch (err) {
-            response.status(500).json({error: err.message});
+            response.status(500).json({ error: err.message });
         }
     }
-
 }
 
 export default UsersController;
