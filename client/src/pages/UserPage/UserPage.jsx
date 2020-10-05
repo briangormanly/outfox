801c625ac6a2b4db6049dba453213477250cf760
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-10-05 19:34:28 -0400

    Scaffolding for UserPage

---
+++ client/src/pages/UserPage/UserPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { UserPageContainer } from './UserPage.elements';
+
+function UserPage() {
+	return (
+		<UserPageContainer>
+			<div>User Page</div>
+		</UserPageContainer>
+	);
+}
+
+export default UserPage;
