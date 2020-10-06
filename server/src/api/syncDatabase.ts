5962959ae406c82116e4e4c418cfd8a6018cfff3
Jason Wilson <jason.wilson2@marist.edu>
2020-10-05 21:16:50 -0400

    Merge remote-tracking branch 'origin/backend' into 'origin/sync'

--- server/src/api/syncDatabase.ts
+++ server/src/api/syncDatabase.ts
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-//Import Sequlize
-//import { Sequelize } from "sequelize";
-import { sequelize } from './databaseConnection';
-// Import all models ['models' are 'Tables' in Sequelize ORM]
-=======
->>>>>>> origin/backend
 import { Category } from "./models/Category";
 import { CategoryTag } from "./models/CategoryTag";
 import { Group } from "./models/Group";
@@ -20,20 +13,8 @@ import { ResourceVersion } from "./models/ResourceVersion";
 import { Tag } from "./models/Tag";
 import { User } from "./models/User";
 
-<<<<<<< HEAD
-/* -------------------------------------------------------
-// Variable for forcing models to sync
-// true -- force models to sync with database - USE ONLY IN DEVOLOPMENT
-// false -- we dont lose data
---------------------------------------------------------- */
-const forceModels = false;
-
-// Array of all models [Tables]
-var models =
-=======
 // Array of all models [Tables]
 const models =
->>>>>>> origin/backend
 [
   User, Group, Tag, Category,
   CategoryTag, GroupCategory,
@@ -42,21 +23,6 @@ const models =
   NoteTag, ResourceTag
 ];
 
-<<<<<<< HEAD
-export const sync = () => {
-  var errorTest = false;
-  for (let i = 0; i < models.length; i++) {
-    (async () => {
-    	await models[i].sync({ force:forceModels }).catch(
-        err => {
-          errorTest = true;
-          console.log("\n\nERROR: " + err + "\n\n")
-      })
-      if (!errorTest) {
-        console.log('\nTABLE SYNCHRONIZED: ' + models[i].name + '\n')
-      }
-      })();
-=======
 // Sync all tables to database
 export const sync = async () => {
   for (const iterator of models) {
@@ -67,7 +33,5 @@ export const sync = async () => {
       .catch((error) => {
         console.log(iterator.name + ' had an error syncing with the database.');
       });
->>>>>>> origin/backend
   }
 }
-
