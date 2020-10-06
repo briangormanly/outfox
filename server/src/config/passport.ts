262a9cf4094140d24b9c190472f5a7472c5ec6c8
AlfyCodes <alfred.ortiz1@marist.edu>
2020-10-06 16:59:21 -0400

    Complete passport.js need to tie in the vars, so it works

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,5 +1,5 @@
-const LocalStrategy = require('passport-local').Strategy;
-const passport = require ('passport')
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
 const sqlize = require('sequelize');
 
 const bcrypt = require('bcryptjs')
@@ -10,7 +10,36 @@ const User = require('../api/models/User')
 
 // Need to check why i need to declare the passport type
 module.exports = function(passport) {
-    passport.use
-
-
+    passport.use(
+      new LocalStrategy({ usernameField: 'username' }, (username, password, done) => {
+        
+        // Match user
+        User.findOne({
+          username: username
+        }).then(user => {
+          if (!user) {
+            return done(null, false, { message: 'That username is not registered' });
+          }
+  
+          // Match password
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: 'Password invalid' });
+            }
+          });
+        });
+      })
+    );
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+    
+      passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+          done(err, user);
+        });
+      });
 }
