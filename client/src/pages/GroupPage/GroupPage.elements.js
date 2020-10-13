bdc0b84ae7dec893f792e9c244bf77fc4ab0c800
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-10-13 00:58:11 -0400

    Add aditional state to Group Page.

--- client/src/pages/GroupPage/GroupPage.elements.js
+++ client/src/pages/GroupPage/GroupPage.elements.js
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { colors, Link } from '../../styles';
 
-const { primary, secondary, white, black, lightGrey } = colors;
+const { primary, primaryLight, secondary, white, black, lightGrey } = colors;
 
 export const GroupPageContainer = styled.div`
 	width: 100%;
@@ -31,6 +31,7 @@ export const Content = styled.div`
 	background-color: ${lightGrey};
 	display: flex;
 	flex-direction: column;
+	align-items: center;
 
 	h1 {
 		margin-top: 1rem;
@@ -38,6 +39,7 @@ export const Content = styled.div`
 		font-size: 2.6rem;
 	}
 	p {
+		margin-top: 0.5rem;
 		text-align: center;
 	}
 `;
@@ -47,9 +49,77 @@ export const ReturnLink = styled(Link)`
   font-size: 2rem;
   display: flex;
   align-items: center;
+  align-self: flex-start;
 
   span{
     display: block;
     margin-left: 1rem;
   }
+`;
+
+export const ButtonContainer = styled.div`
+	width: 70%;
+	height: 3rem;
+	display: flex;
+	justify-content: space-around;
+	margin-top: 1rem;
+`;
+
+export const Button = styled.button`
+	border: none;
+	border-radius: 25px;
+	padding: 0 1rem;
+	color: ${white};
+
+	${(props) =>
+		props.add &&
+		css`
+			background-color: green;
+
+			&:hover {
+				color: green;
+				background-color: ${white};
+				border: 1px solid green;
+			}
+		`};
+
+	${(props) =>
+		props.edit &&
+		css`
+			background-color: ${secondary};
+
+			&:hover {
+				color: ${secondary};
+				background-color: ${white};
+				border: 1px solid ${secondary};
+			}
+		`};
+
+	${(props) =>
+		props.delete &&
+		css`
+			background-color: ${primary};
+
+			&:hover {
+				color: ${primary};
+				background-color: ${white};
+				border: 1px solid ${primary};
+			}
+		`};
+
+	${(props) =>
+		props.flex &&
+		css`
+			display: flex;
+			align-items: center;
+
+			span {
+				padding-left: 0.5rem;
+			}
+		`};
+`;
+
+export const ResourceContainer = styled.div`
+	display: flex;
+	flex-direction: column;
 `;
--- client/src/pages/GroupPage/GroupPage.jsx
+++ client/src/pages/GroupPage/GroupPage.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import groupService from '../../services/groups.js';
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowLeft, FaHammer, FaTrashAlt, FaPlus } from 'react-icons/fa';
 
-import { Loader } from '../../components';
-import { GroupPageContainer, Content, ReturnLink } from './GroupPage.elements';
+import { Loader, Modal } from '../../components';
+import {
+	GroupPageContainer,
+	Content,
+	ReturnLink,
+	ButtonContainer,
+	Button,
+	ResourceContainer
+} from './GroupPage.elements';
 
 const GroupPage = ({ match }) => {
 	const { params: { userID, groupID } } = match;
@@ -13,21 +20,28 @@ const GroupPage = ({ match }) => {
 	const [ resources, setResources ] = useState([]);
 	const [ date, setDate ] = useState('');
 	const [ loading, setLoading ] = useState(false);
+	const [ showAddModal, setShowAddModal ] = useState(false);
+	const [ showEditModal, setShowEditModal ] = useState(false);
+	const [ showDeleteModal, setShowDeleteModal ] = useState(false);
 
-	useEffect(() => {
-		const request = async () => {
-			setLoading(true);
-			const response = await groupService.getGroupData(match.params.groupID);
-			const { datetimeadd, groupdescription, groupname, Resources } = response;
-			setTitle(groupname);
-			setDescription(groupdescription);
-			setResources(Resources);
-			setDate(datetimeadd.slice(0, 10));
-			setLoading(false);
-		};
+	useEffect(
+		() => {
+			const request = async () => {
+				setLoading(true);
+				const response = await groupService.getGroupData(match.params.groupID);
+				const { datetimeadd, groupdescription, groupname, Resources } = response;
+				setTitle(groupname);
+				setDescription(groupdescription);
+				setResources(Resources);
+				setDate(datetimeadd.slice(0, 10));
+				setLoading(false);
+			};
 
-		request();
-	}, []);
+			request();
+		},
+		[ match.params.groupID ]
+	);
+
 	return (
 		<GroupPageContainer>
 			{loading ? (
@@ -39,6 +53,25 @@ const GroupPage = ({ match }) => {
 					</ReturnLink>
 					<h1>{title}</h1>
 					<p>{description}</p>
+					<p>Create Date: {date}</p>
+					<ButtonContainer>
+						<Button add flex>
+							<FaPlus />
+							<span>Add Resource</span>
+						</Button>
+						<Button edit flex>
+							<FaHammer />
+							<span>Edit Group</span>
+						</Button>
+						<Button delete flex>
+							<FaTrashAlt />
+							<span>Delete Group</span>
+						</Button>
+					</ButtonContainer>
+					<ResourceContainer>
+						{/*  */}
+						{/*  */}
+					</ResourceContainer>
 				</Content>
 			)}
 		</GroupPageContainer>
