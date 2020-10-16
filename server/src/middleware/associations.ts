b0be8cc9c9c4be36427da37d05712fe0eae00af9
Jason Wilson <jason.wilson2@marist.edu>
2020-10-16 14:52:18 -0400

    Eddited Associations File

--- server/src/middleware/associations.ts
+++ server/src/middleware/associations.ts
@@ -1,33 +1,28 @@
 import User from "../models/User";
 import Group from "../models/Group";
 import Resource from "../models/Resource";
-
-// Going to be Reconnected once we begin querying
-// import Category from "./Category";
-// import CategoryTag from "./CategoryTag";
-// import GroupCategory from "./GroupCategory";
-// import Link from "./Link";
-// import LinkOwnerType from "./LinkOwnerType";
-// import Note from "./Note";
-// import NoteTag from "./NoteTag";
-// import ResourceTag from "./ResourceTag";
-// import ResourceType from "./ResourceType";
-// import ResourceVersion from "./ResourceVersion";
-// import Tag from "./Tag";
+import Note from "../models/Note";
+import ResourceVersion from "../models/ResourceVersion";
+import Tag from "../models/Tag";
 
 async function Associations(): Promise<void> {
   try {
-    User.hasMany(Group, { foreignKey: "createdby", sourceKey: "id" });
-    Group.belongsTo(User, { foreignKey: "createdby", targetKey: "id" });
+    User.hasMany(Group, { foreignKey: "creatorid", sourceKey: "id" });
+    Group.belongsTo(User, { foreignKey: "creatorid", targetKey: "id" });
 
     User.hasMany(Resource, { foreignKey: "creatorid", sourceKey: "id" });
     Resource.belongsTo(User, { foreignKey: "creatorid", targetKey: "id" });
 
-    Group.hasMany(Resource);
+    //Group.hasMany(Resource);
     Resource.belongsToMany(Group, {
       through: "groupresources",
       timestamps: false,
     });
+    // Group.belongsToMany(Resource, {
+    //   through: "groupresources",
+    //   timestamps: false,
+    // });
+
   } catch (error) {
     throw new Error("Error setting up relationships");
   }
