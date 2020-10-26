4d6914a3461218cd6b7f8262875253c7bb254464
derekreid1 <54563257+derekreid1@users.noreply.github.com>
2020-10-26 16:19:25 -0400

    Added method to user controller

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -3,6 +3,7 @@ import Group from "../models/Group";
 import User from "../models/User";
 import Controller from "../interfaces/ControllerInterface";
 import Resource from "../models/Resource";
+import Friend from "../models/Friend";
 
 /**
  * The user controller is responsible for handling the HTTP requests.
@@ -153,6 +154,29 @@ class UsersController implements Controller {
       response.status(500).send(error.message);
     }
   };
+
+  getUserFriends = async (
+    request: Request,
+    response: Response
+  ): Promise<void> => {
+    const {id} = request.params;
+    try{
+      const userRequestedFriends = await Friend.findAll({where:{requesterid:id, status:"a"}})
+      const userAcceptedFriends = await Friend.findAll({where:{addresseeid:id, status:"a"}})
+
+      const userRequestedFriendsToFriendIds = userRequestedFriends.map(friend=>friend.addresseeid);
+      const userAcceptedFriendsToFriendIds = userAcceptedFriends.map(friend=>friend.requesterid);
+
+      const allUserFriends = [...userRequestedFriendsToFriendIds, ...userAcceptedFriendsToFriendIds];
+
+      response.status(201).json(allUserFriends);
+
+    } catch(err){
+
+    response.status(400).json({ message: "Something went wrong" });
+    }
+  }
+
 }
 
 export default UsersController;
