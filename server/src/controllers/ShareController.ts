09e7d6b12a5e261011547d3bf6be3b40f60cbdfe
Jake Tantorski <jetantor@gmail.com>
2020-10-28 14:14:36 -0400

    Added Shared Resource Methods
    Co-Authored-By: null <JWilson45@users.noreply.github.com>
    Co-Authored-By: Phillip Belilovets <Philltech12@users.noreply.github.com>

--- server/src/controllers/ShareController.ts
+++ server/src/controllers/ShareController.ts
@@ -3,12 +3,12 @@ import ShareGroup from "../models/ShareGroup";
 import ShareResource from "../models/ShareResource";
 
 /**
- * The resource controller is responsible for handling the HTTP requests.
+ * The share controller is responsible for handling the HTTP requests.
  * Examples would be GET, POST, PUT, DELETE.
  */
 class ShareController {
-  // Path that is required in order to access the api http://localhost:8080/routes/api/resources
-  public path = "/api/share";
+  // Path that is required in order to access the api http://localhost:8080/routes/api/share
+  public path = "/api/shared";
   public router = Router();
 
   constructor() {
@@ -16,7 +16,7 @@ class ShareController {
   }
 
   /**
-   * Creates the routes for the resource Controller
+   * Creates the routes for the Share Controller
    * Ex. GET, PUT, POST, UPDATE, etc
    */
   public initializeRoutes(): void {
@@ -42,10 +42,13 @@ class ShareController {
     // Need to add patch
   }
 
-  // Goes to route /api/resources
+  // Goes to route /api/sharedgroup
+
+
+//SHARED GROUP SECTION
 
   /**
-   * Grabs all resources in the database and sends them as a response in json
+   * Grabs all sharedgroup in the database and sends them as a response in json
    * @param request HTTP browser request
    * @param response HTTP browser response
    */
@@ -62,7 +65,7 @@ class ShareController {
   };
 
   /**
-   * Creates a new resources in the database if the request has the correct json
+   * Creates a new sharedgroup in the database if the request has the correct json
    * @param request HTTP browser request
    * @param response HTTP browser response
    */
@@ -79,10 +82,10 @@ class ShareController {
     }
   };
 
-  // Goes to route /api/resources/:id
+  // Goes to route /api/sharedgroup/:id
 
   /**
-   * Grabs a specific resources based off the ID provided
+   * Grabs a specific sharedgroup based off the ID provided
    * @param request HTTP browser request
    * @param response HTTP browser response
    */
@@ -98,7 +101,7 @@ class ShareController {
       } else {
         response
           .status(404)
-          .send("Resource with the specified ID does not exist");
+          .send("Share Group with the specified ID does not exist");
       }
     } catch (error) {
       response.status(500).send(error.message);
@@ -121,12 +124,12 @@ class ShareController {
       }); // Destructure the array so we grab the updated version of our sharedgroup
 
       if (updated) {
-        const updatedShareGroup = await ShareGroup.findOne({ where: { id: id } }); // Grab the update resource
-        response.status(200).json({ resource: updatedShareGroup }); // Return the updated sharedgroup
+        const updatedShareGroup = await ShareGroup.findOne({ where: { id: id } }); // Grab the update sharedgroup
+        response.status(200).json({ sharedgroup: updatedShareGroup ); // Return the updated sharedgroup
       } else {
         response
           .status(404)
-          .send("Resource with the specified ID does not exist"); // sharedgroup does not exist
+          .send("Share Group with the specified ID does not exist"); // sharedgroup does not exist
       }
     } catch (error) {
       response.status(500).send(error.message);
@@ -152,12 +155,147 @@ class ShareController {
       } else {
         response
           .status(404)
-          .send("Resource with the specified ID does not exist");
+          .send("Shared Group with the specified ID does not exist");
       }
     } catch (error) {
       response.status(500).send(error.message);
     }
   };
+
+
+
+//SHARED RESOURCE SECTION
+
+
+
+  /**
+   * Grabs all sharedgroup in the database and sends them as a response in json
+   * @param request HTTP browser request
+   * @param response HTTP browser response
+   */
+  getAllShareResource = async (
+    request: Request,
+    response: Response
+  ): Promise<void> => {
+    try {
+      const sharedresource = await ShareResource.findAll(); // Grabs all share groups
+      response.json(sharedresource);
+    } catch (err) {
+      response.status(400).json({ message: "Something went wrong" });
+    }
+  };
+
+  /**
+   * Creates a new sharedgroup in the database if the request has the correct json
+   * @param request HTTP browser request
+   * @param response HTTP browser response
+   */
+  createShareGroup = async (
+    request: Request,
+    response: Response
+  ): Promise<void> => {
+    try {
+      // If missing non-nullable fields it will create an error
+      const sharedgroup = await ShareGroup.create(request.body);
+      response.status(201).json({ sharedgroup });
+    } catch (error) {
+      response.status(500).send(error.message);
+    }
+  };
+
+  // Goes to route /api/sharedgroup/:id
+
+  /**
+   * Grabs a specific sharedgroup based off the ID provided
+   * @param request HTTP browser request
+   * @param response HTTP browser response
+   */
+  getShareGroup = async (request: Request, response: Response): Promise<void> => {
+    try {
+      const { id } = request.params; // Destructure the request.params object and grab only id
+      const sharedgroup = await ShareGroup.findOne({
+        where: { id: id },
+      }); // Grabs the sharedgroup where the id is 0
+
+      if (sharedgroup) {
+        response.status(200).json(sharedgroup);
+      } else {
+        response
+          .status(404)
+          .send("Share Group with the specified ID does not exist");
+      }
+    } catch (error) {
+      response.status(500).send(error.message);
+    }
+  };
+
+  /**
+   * Updates a sharedgroup based off the ID provided
+   * @param request HTTP browser request
+   * @param response HTTP browser response
+   */
+  updateShareGroup = async (
+    request: Request,
+    response: Response
+  ): Promise<void> => {
+    try {
+      const { id } = request.params; // Destructure the object to only grab the id coming from the request
+      const [updated] = await ShareGroup.update(request.body, {
+        where: { id: id },
+      }); // Destructure the array so we grab the updated version of our sharedgroup
+
+      if (updated) {
+        const updatedShareGroup = await ShareGroup.findOne({ where: { id: id } }); // Grab the update sharedgroup
+        response.status(200).json({ sharedgroup: updatedShareGroup ); // Return the updated sharedgroup
+      } else {
+        response
+          .status(404)
+          .send("Share Group with the specified ID does not exist"); // sharedgroup does not exist
+      }
+    } catch (error) {
+      response.status(500).send(error.message);
+    }
+  };
+
+  /**
+   * Deletes a sharedgroup based off the ID provided
+   * @param request HTTP browser request
+   * @param response HTTP browser response
+   */
+  deleteShareGroup = async (
+    request: Request,
+    response: Response
+  ): Promise<void> => {
+    try {
+      const { id } = request.params; // Destructure the object to only grab the id coming from the request
+      const deleted = await ShareGroup.destroy({
+        where: { id: id },
+      }); // Delete the sharedgroup with the specified id
+      if (deleted) {
+        response.status(204).send("Share Group Deleted");
+      } else {
+        response
+          .status(404)
+          .send("Shared Group with the specified ID does not exist");
+      }
+    } catch (error) {
+      response.status(500).send(error.message);
+    }
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 export default ShareController;
