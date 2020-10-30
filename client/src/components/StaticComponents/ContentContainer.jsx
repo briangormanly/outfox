e67e131577ba8c60996fdc69c81b047dc342fef6
connorfitzpatrick <connorfitz429@gmail.com>
2020-10-30 15:19:08 -0400

    Added components for the content and logo containers

---
+++ client/src/components/StaticComponents/ContentContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../static/css/base.css';
+
+function ContentContainer() {
+    return (
+        <div id = "content_holder">
+        </div>
+    );
+}
+
+export default ContentContainer;
---
+++ client/src/components/StaticComponents/LogoContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../static/css/base.css';
+
+function LogoContainer() {
+    return (
+        <div id = "logoHolder">
+            <img src = "./foxy.svg" id = "fox_image" alt = "logo"></img>
+        </div>
+    );
+}
+
+export default LogoContainer;
