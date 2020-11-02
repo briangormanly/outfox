8d215dca77fa1384fc293b09d204657c6cbb3e92
JPosillico89 <70299482+JPosillico89@users.noreply.github.com>
2020-11-02 14:24:47 -0500

    Update Background.jsx

--- client/src/components/StaticComponents/Background.jsx
+++ client/src/components/StaticComponents/Background.jsx
@@ -5,7 +5,10 @@ const Background = () =>{
     return(
         <Fragment>
             <h2 id = "background" className = "sub_header">Background</h2>
-                <p> This is our capping project, Outfox. The idea for the project came from the desire to have an open source learning platform. What makes Outfox truly unique is that all users have the ability to create and join their own groups with the capability of customizing them with their own resources that everyone can interact with. One of the most important aspects of Outfox is that it offers users the ability to individually tailor their experience based on how they want it.      <br /><br />
+                <p> This is our capping project, Outfox. The idea for the project came from the desire to have an open source learning platform. 
+                    What makes Outfox truly unique is that all users have the ability to create and join their own groups with the capability of 
+                    customizing them with their own resources that everyone can interact with. One of the most important aspects of Outfox is that
+                    it offers users the ability to individually tailor their experience based on how they want it. <br /><br />
 
                   
                 </p>
