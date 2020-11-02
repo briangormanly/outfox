c6d8656c2d9a2112075af3d29cca2a16029deed4
JPosillico89 <70299482+JPosillico89@users.noreply.github.com>
2020-11-02 14:07:58 -0500

    Update Background.jsx

--- client/src/components/StaticComponents/Background.jsx
+++ client/src/components/StaticComponents/Background.jsx
@@ -1,1 +1,17 @@
+import React, { Fragment } from 'react';
+import '../../static/css/base.css';
 
+const Background = () =>{
+    return(
+        <Fragment>
+            <h2 id = "background" className = "sub_header">Background</h2>
+                <p> This is our capping project, Outfox. <br /><br />
+
+                  
+                </p>
+        </Fragment>
+
+    );
+}
+
+export default Background;
