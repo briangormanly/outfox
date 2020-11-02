251481a573950dc6d0b364185378175da3e5cc88
JPosillico89 <70299482+JPosillico89@users.noreply.github.com>
2020-11-02 14:26:18 -0500

    Update Background.jsx

--- client/src/components/StaticComponents/Background.jsx
+++ client/src/components/StaticComponents/Background.jsx
@@ -7,7 +7,7 @@ const Background = () =>{
             <h2 id = "background" className = "sub_header">Background</h2>
                 <p> This is our capping project, Outfox. The idea for the project came from the desire to have an open source learning platform. 
                     What makes Outfox truly unique is that all users have the ability to create and join their own groups with the capability of 
-                    customizing them with their own resources that everyone can interact with. One of the most important aspects of Outfox is that
+                    customizing them with their own resources that everyone can interact with. One of key aspects of Outfox is that
                     it offers users the ability to individually tailor their experience based on how they want it. <br /><br />
 
                   
