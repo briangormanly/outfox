79b9e9c037f848e1297d25b1919ec6232817f5fe
connorfitzpatrick <connorfitz429@gmail.com>
2020-11-03 14:09:09 -0500

    fixed anchor links and table of contents text

--- client/src/components/StaticComponents/TableOfContentsList.jsx
+++ client/src/components/StaticComponents/TableOfContentsList.jsx
@@ -9,14 +9,14 @@ function TableOfContentsList() {
                 <li className = "list_sub_item"><a href = "#background">Background</a></li>
                 <li className = "list_sub_item"><a href = "#mission">Mission</a></li>
                 <li className = "list_sub_item"><a href = "#sign_up">Sign Up</a></li>
-                <li className = "list_sub_item"><a href = "#follow">Follow Outfox</a></li>
-                <li className = "list_item"><a href= "userDocumentation.html">User Documentation</a></li>
-                <li className = "list_sub_item"> <a href = "userDocumentation.html#users">Users</a></li>
-                <li className = "list_sub_item"> <a href = "userDocumentation.html#resources">Resources</a></li>
-                <li className = "list_sub_item"> <a href = "userDocumentation.html#groups">Groups</a></li>
-                <li className = "list_sub_item"> <a href = "userDocumentation.html#friends">Friends</a></li>
-                <li className = "list_sub_item"> <a href = "userDocumentation.html#sharing">Sharing Resources</a></li>
-                <li className = "list_sub_item"> <a href = "userDocumentation.html#onlineTutorials">Online Tutorials</a></li>
+                <li className = "list_sub_item"><a href = "#followoutfox">Follow Outfox</a></li>
+                <li className = "list_item"><a href= "#userDocumentation">User Documentation</a></li>
+                <li className = "list_sub_item"> <a href = "#users">Users</a></li>
+                <li className = "list_sub_item"> <a href = "#resources">Resources</a></li>
+                <li className = "list_sub_item"> <a href = "#groups">Groups</a></li>
+                <li className = "list_sub_item"> <a href = "#friends">Friends</a></li>
+                <li className = "list_sub_item"> <a href = "#sharing">Sharing Resources</a></li>
+                <li className = "list_sub_item"> <a href = "#onlineTutorials">Online Tutorials</a></li>
             </ul>
         </div>
     );
