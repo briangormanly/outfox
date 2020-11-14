30bdea2c1b32ce5c6df409bbc633ff8329e66a6a
stillmoon <jgustafson1997@hotmail.com>
2020-11-14 14:47:07 -0500

    Added Routing for Uploading Files

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 // Cors is only being used if we run React separately
 import cors from "cors";
 // Using Morgan for middleware. At the moment for basic logging
@@ -11,7 +11,7 @@ import bodyParser from "body-parser";
 import session from "express-session";
 import Controller from "./interfaces/ControllerInterface";
 import validLogin from "./middleware/validLogin";
-
+import fileUpload from "express-fileupload";
 /**
  * Used as the primarily class for the express server
  */
@@ -59,6 +59,7 @@ class App {
       })
     );
     this.app.use(cookieParser("secretcode"));
+    this.app.use(fileUpload());
   }
 
   private initializeControllers(controllers: Controller[]): void {
@@ -67,7 +68,30 @@ class App {
       res.send("Do not use .render() this isn't ejs");
     });
     this.app.post("/login", validLogin);
+    this.app.post(
+      "/upload",
+      (request: Request, response: Response): unknown => {
+        if (request.files === null) {
+          return response.status(400).json({ msg: "No File Uploaded" });
+        }
 
+        const file = request.files.file;
+        file.mv(
+          `${__dirname}/client/public/uploads/${file.name}`,
+          (error: Error) => {
+            if (error) {
+              console.error(error);
+              return response.status(500).send(error);
+            }
+
+            response.json({
+              fileName: file.name,
+              filePath: `/uploads/${file.name}`,
+            });
+          }
+        );
+      }
+    );
     for (const iterator of controllers) {
       this.app.use(iterator.router);
     }
