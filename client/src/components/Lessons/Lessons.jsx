712ce6befee6a8b83a62a7cd87c4d633f1327c86
Test <test@148-100-130-220.foxnet.marist.edu>
2021-10-13 19:33:36 -0400

    Added no lessons page

--- client/src/components/Lessons/Lessons.jsx
+++ client/src/components/Lessons/Lessons.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import {
+  LessonsContainer,
+  TitleContainer,
+  InnerContainer,
+  Content,
+  VerticalLine,
+} from "./Lessons.elements";
+
+import { FaClipboard } from "react-icons/fa";
 
 const Lessons = () => {
-	return (
-		<div>
-			<h1>Lessons</h1>
-		</div>
-	);
+  return (
+    <LessonsContainer>
+      <button> Create Lesson</button>
+      <TitleContainer>
+        <h1>My Lessons</h1>
+        <InnerContainer>
+          <Content>
+            <VerticalLine />
+            <FaClipboard />
+            <p> You do not have any lessons</p>
+            <button> Create Lesson</button>
+          </Content>
+        </InnerContainer>
+      </TitleContainer>
+  </LessonsContainer>
+  );
 };
 
 export default Lessons;
