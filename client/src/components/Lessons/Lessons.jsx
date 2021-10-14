b49001cfe9b72a108367b8546b2e13c21f0d6826
scordoni <54602744+scordoni@users.noreply.github.com>
2021-10-13 21:52:58 -0400

    Addition of Create Lesson Form

--- client/src/components/Lessons/Lessons.jsx
+++ client/src/components/Lessons/Lessons.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+import { useState } from "react";
+
 import {
   LessonsContainer,
   TitleContainer,
@@ -7,25 +10,53 @@ import {
   VerticalLine,
 } from "./Lessons.elements";
 
-import { FaClipboard } from "react-icons/fa";
+import { Modal, CreateLessonForm, ResourceCard } from "../index";
+import { FaPlus, FaArrowRight, FaClipboard } from "react-icons/fa";
 
-const Lessons = () => {
-  return (
-    <LessonsContainer>
-      <button> Create Lesson</button>
-      <TitleContainer>
-        <h1>My Lessons</h1>
-        <InnerContainer>
-          <Content>
-            <VerticalLine />
-            <FaClipboard />
-            <p> You do not have any lessons</p>
-            <button> Create Lesson</button>
-          </Content>
-        </InnerContainer>
-      </TitleContainer>
-  </LessonsContainer>
-  );
-};
+import { useSelector } from "react-redux";
+
+
+function Lessons({ dashboardPaginate }) {
+
+    const { user } = useSelector((state) => state.userDetail);
+    const [showModal, setShowModal] = useState(false);
+    const { id, Resources } = user;
+
+    const plus = { color: "white", fontSize: "2.5em"}
+
+    
+    return (
+        <React.Fragment>
+        {showModal && (
+            <Modal large setShowModal={setShowModal} >
+            <CreateLessonForm creatorid={id} setShowModal={setShowModal} />
+            </Modal>
+        )}
+
+        <LessonsContainer>
+        
+            <button onClick={() => setShowModal(true)}>
+                <span>Create Lesson</span> <FaPlus />
+            </button>
+        
+        <TitleContainer>
+            <h1>My Lessons</h1>
+            <InnerContainer>
+            <Content>
+                <VerticalLine />
+                <FaClipboard />
+                <p> You do not have any lessons</p>
+                <button onClick={() => setShowModal(true)}>
+                <span>Create Lesson</span> <FaPlus style={plus} />
+                </button>
+            </Content>
+            </InnerContainer>
+        </TitleContainer>
+    </LessonsContainer>
+    </React.Fragment>
+    );
+    
+
+}
 
 export default Lessons;
