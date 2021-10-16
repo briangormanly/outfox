c3d77e08ef3426f57479da37d281be456ebf8302
scordoni <54602744+scordoni@users.noreply.github.com>
2021-10-16 16:13:26 -0400

    Assignment Lesson Elements

--- client/src/components/AssignmentLesson/AssignmentLesson.elements.js
+++ client/src/components/AssignmentLesson/AssignmentLesson.elements.js
@@ -40,3 +40,62 @@ export const AssignmentContainer = styled.div`
     }
   }
 `;
+
+export const TitleContainer = styled.div`
+  min-height: 65vh;
+  width: 90%;
+  padding: 3rem;
+  margin: 0 auto;
+  margin-bottom: 5rem;
+  background-color: ${primary};
+  border-top: solid ${primary} 10px;
+  border-radius: 5px;
+
+  h1 {
+    color: ${white};
+  }
+`;
+
+export const InnerContainer = styled.div`
+  width: 100%;
+  padding: 3rem;
+  margin: 0 auto;
+`;
+
+export const Content = styled.div`
+  min-height: 55vh;
+  width: 100%;
+  border-radius: 5px;
+  padding: 2rem;
+  background-color: ${white};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-shadow: inset 4px 4px 4px 2px rgba(0, 0, 0, 0.25);
+
+  svg {
+    font-size: 10rem;
+    color: rgba(229, 229, 229, 0.5);
+  }
+
+  p {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  button {
+    margin-top: 0;
+    margin-right: 0;
+    align-self: center;
+  }
+`;
+
+export const VerticalLine = styled.div`
+  border: 2px solid rgba(229, 229, 229, 0.5);
+  position: relative;
+  transform: rotate(48deg);
+  border-left: 2px solid ${white};
+  top: 125px;
+  height: 150px;
+`;
