739ad2c3357b54cd4c8216844c41c8097f1a464e
scordoni <54602744+scordoni@users.noreply.github.com>
2021-10-16 15:53:02 -0400

    Resource Lesson

---
+++ client/src/components/ResourceLesson/ResourceLesson.jsx
@@ -0,0 +1,44 @@
+import React, { Fragment, useRef, useState } from "react";
+import {
+  ResourceContainer,
+  TitleContainer,
+  InnerContainer,
+  Content,
+  VerticalLine,
+} from "./ResourceLessons.elements";
+
+import { FaClipboard } from "react-icons/fa";
+import {Modal, AddResourceForm} from "../index";
+
+const ResourceLessons = () => {
+
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <Fragment>
+      {showModal && (
+        <Modal large setShowModal={setShowModal}>
+          <AddResourceForm setShowModal={setShowModal} />
+        </Modal>
+      )}
+      
+        
+          <h1>My Resources</h1>
+          <InnerContainer>
+            <Content>
+              <VerticalLine />
+              <FaClipboard />
+              <p> You do not have any Reources</p>
+              <button onClick={() => setShowModal(true)}>
+                {" "}
+                Create {" "}
+              </button>
+            </Content>
+          </InnerContainer>
+            
+      
+    </Fragment>
+  );
+};
+
+export default ResourceLessons;
