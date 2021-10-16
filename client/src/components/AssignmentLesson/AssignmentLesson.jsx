6bbdfd0a0e1c7035dabd95457bf804a5fc80be29
scordoni <54602744+scordoni@users.noreply.github.com>
2021-10-16 15:58:05 -0400

    Assignment Lesson

---
+++ client/src/components/AssignmentLesson/AssignmentLesson.jsx
@@ -0,0 +1,48 @@
+import React, { Fragment, useRef, useState } from "react";
+import {
+  AssignmentContainer,
+  TitleContainer,
+  InnerContainer,
+  Content,
+  VerticalLine,
+} from "./AssignmentLessons.elements";
+
+import { FaClipboard } from "react-icons/fa";
+import { CreateAssignmentForm, Modal, AssignmentCard } from "../index";
+
+const AssignmentLessons = () => {
+
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <Fragment>
+      {showModal && (
+        <Modal setShowModal={setShowModal}>
+          <CreateAssignmentForm setShowModal={setShowModal} />
+        </Modal>
+      )}
+     
+        
+       
+          <h1>My Assignments</h1>
+          <InnerContainer>
+            <Content>
+              <VerticalLine />
+              <FaClipboard />
+              <p> You do not have any assignments</p>
+              <button onClick={() => setShowModal(true)}>
+                {" "}
+                Create Assignment{" "}
+              </button>
+            </Content>
+          </InnerContainer>
+      
+
+     
+
+    
+    </Fragment>
+  );
+};
+
+export default AssignmentLessons;
