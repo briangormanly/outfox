4e54842747c33515ed560f1e9b94f2e041267d31
scordoni <54602744+scordoni@users.noreply.github.com>
2021-10-16 16:00:41 -0400

    Lesson Services

--- client/src/services/lesson.js
+++ client/src/services/lesson.js
@@ -1,3 +1,36 @@
 import axios from 'axios';
 
-const lessonURL = 'http://localhost:8080/api/lessons';
+const LessonURL = 'http://localhost:8080/api/lessons';
+
+
+// Lessons
+const createLesson= async (newLessonObject) => {
+    const response = await axios.post(LessonURL, newLessonObject, {
+        headers : {
+            'Content-Type' : 'multipart/form-data'
+        }
+    });
+    return response.data;
+};
+
+const deleteLesson = async (id) => {
+    const response = await axios.delete(`${LessonURL}/${id}`);
+    return response.data;
+};
+
+const editLesson = async (id, newObject) => {
+    const response = await axios.put(`${LessonURL}/${id}`, newObject);
+    return response.data;
+};
+
+const getLessonData = async (id) => {
+    const response = await axios.get(`${LessonURL}/${id}`);
+    return response.data;
+};
+
+export default {
+    createLesson,
+    deleteLesson,
+    editLesson,
+    getLessonData
+};
