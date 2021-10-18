586feafc9e9ea4b5bbaca6fdc889b411d7b05e10
kaylinm <kaylin.moss1@marist.edu>
2021-10-18 15:01:15 -0400

    updated assignment card to tie in back end

--- client/src/components/AssignmentCard/AssignmentCard.elements.js
+++ client/src/components/AssignmentCard/AssignmentCard.elements.js
@@ -4,7 +4,7 @@ import { colors } from "../../styles";
 
 const { white, primary, secondary, black } = colors;
 
-export const AssignmentCardContainer = styled.div`
+/*export const AssignmentCardContainer = styled.div`
   #start {
     background: yellow;
   }
@@ -171,4 +171,65 @@ export const ReceiverResourceContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+`;*/
+
+export const Card = styled.div`
+  height: 90%;
+  width: 25rem;
+  flex: 0 0 auto;
+  align-self: flex-end;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+
+  background-color: ${primary};
+  /* border: 1px solid black; */
+  border-radius: 16px;
+  color: white;
+
+  -webkit-box-shadow: -29px 6px 15px -10px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: -29px 6px 15px -10px rgba(0, 0, 0, 0.75);
+  box-shadow: -29px 6px 15px -10px rgba(0, 0, 0, 0.75);
+
+  transition: 0.2s;
+
+  &:hover {
+    transform: translateY(-1.5rem);
+  }
+
+  &:hover ~ div {
+    transform: translateX(80px);
+  }
+
+  &:not(:first-child) {
+    margin-left: -80px;
+  }
+
+  h2 {
+    font-size: 2.5rem;
+  }
+
+  p {
+    margin-top: 2rem;
+    font-size: 1.6rem;
+  }
+
+  button {
+    display: block;
+    margin-top: auto;
+    margin-bottom: 2rem;
+
+    font-family: inherit;
+    font-size: 1.6rem;
+    padding: 0.7rem 1rem;
+    cursor: pointer;
+    background-color: ${secondary};
+    border: none;
+    border-radius: 5px;
+    color: ${white};
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
 `;
--- client/src/components/AssignmentCard/AssignmentCard.jsx
+++ client/src/components/AssignmentCard/AssignmentCard.jsx
@@ -1,130 +1,151 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { useHistory, useParams } from "react-router-dom";
+
+import { Card } from "./AssignmentCard.elements";
+
+/*import { ProgressBar, Step } from "react-step-progress-bar";
 import "react-step-progress-bar/styles.css";
-import { ProgressBar, Step } from "react-step-progress-bar";
 
 import { colors } from "../../styles";
 
 import {
-  AssignmentCardContainer,
-  AssignmentCardContent,
-  TitleContainer,
-  StatusBarContainer,
-  OwnerResourceContainer,
-  ReceiverResourceContainer,
-  DatesDescriptionActionContainer,
-  DatesContainer,
-  DatesContainerQuestions,
-  DatesContainerAnswers,
-  DescriptionContainer,
-  ActionContainer,
-  ViewResourceButton,
-  SubmitButton,
+    AssignmentCardContainer,
+    AssignmentCardContent,
+    TitleContainer,
+    StatusBarContainer,
+    OwnerResourceContainer,
+    ReceiverResourceContainer,
+    DatesDescriptionActionContainer,
+    DatesContainer,
+    DatesContainerQuestions,
+    DatesContainerAnswers,
+    DescriptionContainer,
+    ActionContainer,
+    ViewResourceButton,
+    SubmitButton,
 } from "./AssignmentCard.elements";
 
 import {
-  FaAngleLeft,
-  FaPencilAlt,
-  FaComments,
-  FaShare,
-  FaTrashAlt,
-} from "react-icons/fa";
+    FaAngleLeft,
+    FaPencilAlt,
+    FaComments,
+    FaShare,
+    FaTrashAlt,
+} from "react-icons/fa";*/
 
-const AssignmentCard = (props) => {
-  const isOwner = props.isOwner;
-  const setShowSubmitAssignmentModal = props.setShowSubmitAssignmentModal;
+/*const AssignmentCard = (props) => {
+    const isOwner = props.isOwner;
+    const setShowSubmitAssignmentModal = props.setShowSubmitAssignmentModal;
 
-  const { secondary } = colors;
-  const statusBarSteps = document.getElementsByClassName("RSPBstep");
+    const { secondary } = colors;
+    const statusBarSteps = document.getElementsByClassName("RSPBstep");
 
-  //fixes styling issue for status bar
-  for (let i = 0; i < statusBarSteps.length; i++) {
-    if (statusBarSteps[i].style.left === "0%") {
-      statusBarSteps[i].style.left = "10%";
+    //fixes styling issue for status bar
+    for (let i = 0; i < statusBarSteps.length; i++) {
+        if (statusBarSteps[i].style.left === "0%") {
+            statusBarSteps[i].style.left = "10%";
+        }
+        if (statusBarSteps[i].style.left === "100%") {
+            statusBarSteps[i].style.left = "90%";
+        }
     }
-    if (statusBarSteps[i].style.left === "100%") {
-      statusBarSteps[i].style.left = "90%";
-    }
-  }
+
+    return (
+        <AssignmentCardContainer>
+            <AssignmentCardContent>
+                <TitleContainer>
+                    <FaAngleLeft />
+                    <h1> Assignment Name</h1>
+                </TitleContainer>
+                <StatusBarContainer>
+                    <ProgressBar
+                        percent={25}
+                        filledBackground={secondary}
+                        unfilledBackground={"#757575"}
+                    >
+                        <Step transition="scale" id={"start"}>
+                            {({ accomplished }) => <span> Open</span>}
+                        </Step>
+                        <Step transition="scale">
+                            {({ accomplished }) => <span> Closed</span>}
+                        </Step>
+                        <Step transition="scale" className={"end"}>
+                            {({ accomplished }) => <span> Returned</span>}
+                        </Step>
+                    </ProgressBar>
+                </StatusBarContainer>
+                <DatesDescriptionActionContainer>
+                    <DatesContainer>
+                        <DatesContainerQuestions>
+                            <p>
+                                <strong>Open Date:</strong>
+                            </p>
+                            <p>
+                                <strong>Due Date:</strong>
+                            </p>
+                            <p>
+                                <strong>Close Date:</strong>
+                            </p>
+                            <p>
+                                <strong> Grade:</strong>
+                            </p>
+                        </DatesContainerQuestions>
+                        <DatesContainerAnswers>
+                            <p>9/22/21</p>
+                            <p>9/22/21</p>
+                            <p>9/22/21</p>
+                            <p>Not Graded</p>
+                        </DatesContainerAnswers>
+                    </DatesContainer>
+
+                    <DescriptionContainer>
+                        <p>Assignment Description</p>
+                    </DescriptionContainer>
+
+                    <ActionContainer>
+                        <FaComments />
+                        {isOwner && (
+                            <Fragment>
+                                <FaPencilAlt />
+
+                                <FaShare />
+                                <FaTrashAlt />
+                            </Fragment>
+                        )}
+                    </ActionContainer>
+                </DatesDescriptionActionContainer>
+                {isOwner ? (
+                    <OwnerResourceContainer>
+                        <ViewResourceButton> View Resource </ViewResourceButton>
+                    </OwnerResourceContainer>
+                ) : (
+                    <ReceiverResourceContainer>
+                        <ViewResourceButton> View Resource </ViewResourceButton>
+                        <SubmitButton onClick={() => setShowSubmitAssignmentModal(true)}>
+                            {" "}
+                            Submit Assignment{" "}
+                        </SubmitButton>
+                    </ReceiverResourceContainer>
+                )}
+            </AssignmentCardContent>
+        </AssignmentCardContainer>
+    );
+};*/
+
+const AssignmentCard = ({ name, description, id }) => {
+  const history = useHistory();
+  const params = useParams();
+
+  const handleClick = () => {
+    history.push(`/user/${params.id}/assignments/${id}`);
+  };
 
   return (
-    <AssignmentCardContainer>
-      <AssignmentCardContent>
-        <TitleContainer>
-          <FaAngleLeft />
-          <h1> Assignment Name</h1>
-        </TitleContainer>
-        <StatusBarContainer>
-          <ProgressBar
-            percent={25}
-            filledBackground={secondary}
-            unfilledBackground={"#757575"}
-          >
-            <Step transition="scale" id={"start"}>
-              {({ accomplished }) => <span> Open</span>}
-            </Step>
-            <Step transition="scale">
-              {({ accomplished }) => <span> Closed</span>}
-            </Step>
-            <Step transition="scale" className={"end"}>
-              {({ accomplished }) => <span> Returned</span>}
-            </Step>
-          </ProgressBar>
-        </StatusBarContainer>
-        <DatesDescriptionActionContainer>
-          <DatesContainer>
-            <DatesContainerQuestions>
-              <p>
-                <strong>Open Date:</strong>
-              </p>
-              <p>
-                <strong>Due Date:</strong>
-              </p>
-              <p>
-                <strong>Close Date:</strong>
-              </p>
-              <p>
-                <strong> Grade:</strong>
-              </p>
-            </DatesContainerQuestions>
-            <DatesContainerAnswers>
-              <p>9/22/21</p>
-              <p>9/22/21</p>
-              <p>9/22/21</p>
-              <p>Not Graded</p>
-            </DatesContainerAnswers>
-          </DatesContainer>
-
-          <DescriptionContainer>
-            <p>Assignment Description</p>
-          </DescriptionContainer>
-
-          <ActionContainer>
-            <FaComments />
-            {isOwner && (
-              <Fragment>
-                <FaPencilAlt />
-
-                <FaShare />
-                <FaTrashAlt />
-              </Fragment>
-            )}
-          </ActionContainer>
-        </DatesDescriptionActionContainer>
-        {isOwner ? (
-          <OwnerResourceContainer>
-            <ViewResourceButton> View Resource </ViewResourceButton>
-          </OwnerResourceContainer>
-        ) : (
-          <ReceiverResourceContainer>
-            <ViewResourceButton> View Resource </ViewResourceButton>
-            <SubmitButton onClick={() => setShowSubmitAssignmentModal(true)}>
-              {" "}
-              Submit Assignment{" "}
-            </SubmitButton>
-          </ReceiverResourceContainer>
-        )}
-      </AssignmentCardContent>
-    </AssignmentCardContainer>
+    <Card>
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <button onClick={handleClick}>View Assignment</button>
+    </Card>
   );
 };
 
