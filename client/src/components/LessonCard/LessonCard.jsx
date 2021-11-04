3e3e3a7f48157e1e3b3aa98ab7ce76ad43b5be93
scordoni <54602744+scordoni@users.noreply.github.com>
2021-11-03 21:14:54 -0400

    Lesson Card

--- client/src/components/LessonCard/LessonCard.jsx
+++ client/src/components/LessonCard/LessonCard.jsx
@@ -1,7 +1,9 @@
 import React, { useState, Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { ResourceCard } from '../index';
 
 import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
 
@@ -16,13 +18,16 @@ import userService from '../../services/users';
 
 import { deleteSharedResource } from '../../redux/actions/userActions';
 
-import {
+//import {i} from' ../../components/ResourceLesson/ResourceLesson';
 
+import {
+    LessonsContainer,
     Description, 
     
 } from './LessonCard.elements';
 
 import { ActionButton as Button } from '../../styles';
+import {Modal, EditLessonForm, DeleteLessonForm} from "../index";
 
 
 
@@ -40,36 +45,64 @@ const LessonCard = ({
     showDates
 }) => {
    
+    const { user } = useSelector((state) => state.userDetail);
+    const {
+        user: { Lessons },
+      } = useSelector((state) => state.userDetail);
+    const [showModal, setShowModal] = useState(false);
+
+    const { Resources } = user;
+    
+
+    const [ showEditModal, setShowEditModal ] = useState(false);
+    const [ showDeleteModal, setShowDeleteModal ] = useState(false);
 
     const params = useParams();
 
     // redux
     const dispatch = useDispatch();
 
-
     return (
-        <Fragment>
+        <React.Fragment>
+        {showEditModal && (
+            <Modal large setShowModal={setShowEditModal}>
+            <EditLessonForm setShowModal={setShowEditModal} lessonID={id} />
+            </Modal>
+        )}
+        {showDeleteModal && (
+            <Modal setShowModal={setShowDeleteModal}>
+            <DeleteLessonForm setShowModal={setShowDeleteModal} lessonID={id} />
+            </Modal>
+        )}
+
+            <h1>{title}</h1>
+            <React.Fragment>
+            <LessonsContainer>
+                <button edit = "true" onClick={() => setShowEditModal(true)}>
+                Edit
+                </button>
             
-              
-                    
-                        <h2>{title}</h2>
-                        {showDescription && (
-                            <Description>
-                                {parse(description)}
-                            </Description>
-                        )}
+                <button delete = "true" onClick={() => setShowDeleteModal(true)}>
+                Delete
+                </button>
 
-
-                    
-              
-
+                <button onClick={() => setShowDeleteModal(true)}>
+                Share
+                </button>
+                </LessonsContainer>
+            </React.Fragment>
+            
            
-        </Fragment>
+            
+            {showDescription && (
+             <Description>
+                {parse(description)}
+             </Description>
+             )}
+
+            
+        </React.Fragment>
     );
 };
-
-
-
-
 
 export default LessonCard;
