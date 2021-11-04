618c3921137327c1999d0a6d1c99fdb0939f29b8
scordoni <54602744+scordoni@users.noreply.github.com>
2021-11-03 21:15:07 -0400

    Lesson Card

--- client/src/components/LessonCard/LessonCard.elements.js
+++ client/src/components/LessonCard/LessonCard.elements.js
@@ -7,3 +7,31 @@ const { primary, secondary, mediumGrey, white } = colors;
 
 
 export const Description = styled.div`font-size: 1.4rem;`;
+
+export const LessonsContainer = styled.div`
+  
+button {
+    margin-top: 5rem;
+    margin-bottom: 5rem;
+    font-family: inherit;
+    font-size: 1.6rem;
+    padding: 0.7rem 1rem;
+    display: flex;
+    flexDirection: row;
+    justify-content: space-around;
+    
+    cursor: pointer;
+    background-color: ${secondary};
+    border: 1px solid ${secondary};
+    border-radius: 5px;
+    color: ${white};
+
+    &:hover {
+      color: ${secondary};
+      background-color: ${white};
+      border: 1px solid ${secondary};
+    }
+  
+    
+`;
+
