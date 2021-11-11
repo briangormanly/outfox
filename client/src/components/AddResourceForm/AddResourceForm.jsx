0e36d45baf87bbdbf8557fdfc5c04c6a94f46f87
kaylinm <kaylin.moss1@marist.edu>
2021-11-10 19:46:16 -0500

    fixed edit assignment bug

--- client/src/components/AddResourceForm/AddResourceForm.jsx
+++ client/src/components/AddResourceForm/AddResourceForm.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { addUserResource } from "../../redux/actions/userActions";
 import { addGroupResource } from "../../redux/actions/groupPageActions";
 import { addnewLessonResource } from "../../redux/actions/lessonsActions";
+import { addAssignmentResource } from "../../redux/actions/assignmentActions";
 import FormInput from "../Form-Input/Form-Input";
 
 import ReactQuill from "react-quill";
@@ -35,6 +36,7 @@ function reducer(state, { field, value }) {
 const AddResourceForm = ({
   creatorid,
   GroupId,
+  AssignmentId,
   LessonId,
   setShowModal,
   isWithAssignments,
@@ -99,6 +101,9 @@ const AddResourceForm = ({
     console.log("Group ID:");
     console.log(GroupId);
 
+    console.log("Assignment ID");
+    console.log(AssignmentId);
+
     console.log("Lesson ID:");
     console.log(LessonId);
 
@@ -115,6 +120,11 @@ const AddResourceForm = ({
       newObject = { ...state, mutable: true, LessonId: LessonId };
     }
 
+    if (AssignmentId) {
+      formData.append("AssignmentId", AssignmentId);
+      newObject = { ...state, mutable: true, AssignmentId: AssignmentId };
+    }
+
     if (creatorid) {
       formData.append("creatorid", creatorid);
       newObject = { ...state, mutable: true, creatorid: creatorid };
@@ -124,14 +134,11 @@ const AddResourceForm = ({
       if (GroupId) {
         // storeDispatch(addGroupResource(newObject));
         storeDispatch(addGroupResource(formData));
-      } 
-      
-      else if (LessonId){
-        
+      } else if (LessonId) {
         storeDispatch(addnewLessonResource(formData));
-      } 
-      
-      else {
+      } else if (AssignmentId) {
+        storeDispatch(addAssignmentResource(formData));
+      } else {
         // storeDispatch(addUserResource(newObject));
         storeDispatch(addUserResource(formData));
       }
@@ -150,11 +157,11 @@ const AddResourceForm = ({
     setFileName(e.target.files[0].name);
   };
 
-  const togglePagesTwoThree = (e) => {
-    let page2 = document.getElementById("create-assignment-page2");
-    page2.classList.toggle("is-active");
+  const togglePagesOneThree = (e) => {
+    let page1 = document.getElementById("edit-assignment-page1");
+    page1.classList.toggle("is-active");
 
-    let page3 = document.getElementById("create-assignment-page3");
+    let page3 = document.getElementById("edit-assignment-page3");
     page3.classList.toggle("is-active");
   };
   return (
@@ -229,7 +236,7 @@ const AddResourceForm = ({
             fullWidth
             type="submit"
             value="Upload"
-            onClick={togglePagesTwoThree}
+            onClick={togglePagesOneThree}
           >
             Create Resource
           </ActionButton>
