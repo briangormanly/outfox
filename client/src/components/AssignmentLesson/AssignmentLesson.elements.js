2ded2f51a7b4ef40417bcf902e7d97b66e4c3518
kaylinm <kaylin.moss1@marist.edu>
2021-11-29 16:17:58 -0500

    modified assignment lessons so that add only shows if user has an assignment

--- client/src/components/AssignmentLesson/AssignmentLesson.elements.js
+++ client/src/components/AssignmentLesson/AssignmentLesson.elements.js
@@ -4,36 +4,42 @@ import { colors } from "../../styles";
 
 const { primary, secondary, white, black } = colors;
 
-export const AssignmentContainer = styled.div`
-  
-  
-  
-
+export const NoAssignmentsContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+  color: rgba(255, 255, 255, 1);
 
+  svg {
+    color: rgba(255, 255, 255, 1);
+  }
+  
   
   }
 `;
 
-
-export const AssignmentContainer1 = styled.div`
-min-height: 3vh;
-width: 100%;
-padding: 2rem;
-background-color: ${white};
-border: 1px solid ${black};
-display: flex;
-flex-direction: column;
-justify-content: center;
-  
+export const AssignmentsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
-
+export const AddAssignmentButton = styled.button`
+  width: 100%;
+`;
+export const SelectAssignmentContainer = styled.div`
+  min-height: 3vh;
+  width: 100%;
+  padding: 2rem;
+  background-color: ${white};
+  border: 1px solid ${black};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 export const InnerContainer = styled.div`
   width: 100%;
- 
+
   margin: 0 auto;
 `;
 
@@ -42,22 +48,61 @@ export const Content = styled.div`
   width: 100%;
   border-radius: 5px;
   padding: 2rem;
-  background-color: ${white};
+
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  box-shadow: inset 4px 4px 4px 2px rgba(0, 0, 0, 0.25);
-
-  svg {
-    font-size: 10rem;
-    color: rgba(229, 229, 229, 0.5);
-  }
 
   p {
     margin-top: 20px;
     margin-bottom: 20px;
   }
+
+  button {
+    font-family: inherit;
+    font-size: 1.6rem;
+    padding: 0.7rem 1rem;
+    display: flex;
+    margin-left: 1rem;
+    justify-content: space-around;
+    align-items: center;
+    cursor: pointer;
+
+    border: none;
+    border-radius: 5px;
+    color: ${white};
+
+    &:hover {
+      color: ${secondary};
+      background-color: ${white};
+      border: 1px solid ${secondary};
+    }
+
+    span {
+      display: inline-block;
+      margin-right: 1rem;
+      min-width: 5rem;
+    }
+  }
+`;
+
+export const VerticalLine = styled.div`
+  border: 2px solid rgba(229, 229, 229, 0.5);
+  position: relative;
+  transform: rotate(48deg);
+  border-left: 2px solid ${white};
+  top: 125px;
+  height: 150px;
+`;
+
+export const SelectButtonContainer = styled.div`
+  width: 35rem;
+
+  display: flex;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: center;
 
   button {
     font-family: inherit;
@@ -74,62 +119,15 @@ export const Content = styled.div`
     color: ${white};
 
     &:hover {
-        color: ${secondary};
-        background-color: ${white};
-        border: 1px solid ${secondary};
+      color: ${secondary};
+      background-color: ${white};
+      border: 1px solid ${secondary};
     }
 
     span {
-        display: inline-block;
-        margin-right: 1rem;
-        min-width: 5rem;
+      display: inline-block;
+      margin-right: 1rem;
+      min-width: 5rem;
     }
-}
-  
-`;
-
-export const VerticalLine = styled.div`
-  border: 2px solid rgba(229, 229, 229, 0.5);
-  position: relative;
-  transform: rotate(48deg);
-  border-left: 2px solid ${white};
-  top: 125px;
-  height: 150px;
-`;
-
-
-export const ButtonContainer = styled.div`
-    width: 35rem;
-
-    display: flex;
-    height: 100%;
-    justify-content: flex-end;
-    align-items: center;
-
-    button {
-        font-family: inherit;
-        font-size: 1.6rem;
-        padding: 0.7rem 1rem;
-        display: flex;
-        margin-left: 1rem;
-        justify-content: space-around;
-        align-items: center;
-        cursor: pointer;
-        background-color: ${primary};
-        border: none;
-        border-radius: 5px;
-        color: ${white};
-
-        &:hover {
-            color: ${secondary};
-            background-color: ${white};
-            border: 1px solid ${secondary};
-        }
-
-        span {
-            display: inline-block;
-            margin-right: 1rem;
-            min-width: 5rem;
-        }
-    }
+  }
 `;
--- client/src/components/AssignmentLesson/AssignmentLesson.jsx
+++ client/src/components/AssignmentLesson/AssignmentLesson.jsx
@@ -1,28 +1,28 @@
 import React, { Fragment, useState } from "react";
-import {addLessonAssignment} from "../../redux/actions/lessonsActions";
-import {createLessonAction} from "../../redux/actions/userActions";
+import { addLessonAssignment } from "../../redux/actions/lessonsActions";
+import { createLessonAction } from "../../redux/actions/userActions";
 import {
-  AssignmentContainer,
-  AssignmentContainer1,
+  NoAssignmentsContainer,
+  AssignmentsContainer,
+  SelectAssignmentContainer,
+  AddAssignmentButton,
   InnerContainer,
   Content,
   VerticalLine,
-  ButtonContainer,
+  SelectButtonContainer,
 } from "./AssignmentLesson.elements";
 
-import lessonService from '../../services/lesson.js';
+import lessonService from "../../services/lesson.js";
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
 import { FaClipboard } from "react-icons/fa";
 import { CreateAssignmentForm, Modal } from "../index";
 
-
-const AssignmentLesson = ({lessonID, setShowModal}) => {
-
+const AssignmentLesson = ({ lessonID, setShowModal }) => {
   const { user } = useSelector((state) => state.userDetail);
   const [showAssignmentModal, setShowAssignmentModal] = useState(false);
-  const style = { color: "black"};
+  const style = { color: "black" };
 
   const { Assignments } = user;
 
@@ -34,8 +34,8 @@ const AssignmentLesson = ({lessonID, setShowModal}) => {
     console.log(assignmentID);
     console.log(lessonID);
 
-    const { Assignments, description, title} = response;
-  
+    const { Assignments, description, title } = response;
+
     const newObject = {
       Assignments,
       description,
@@ -45,59 +45,58 @@ const AssignmentLesson = ({lessonID, setShowModal}) => {
 
     dispatch(createLessonAction(newObject, Assignments));
   };
-  
-  
+
   return (
     <Fragment>
       {showAssignmentModal && (
         <Modal setShowModal={setShowAssignmentModal}>
-          <CreateAssignmentForm lessonID = {lessonID} setShowModal={setShowAssignmentModal} />
+          <CreateAssignmentForm
+            lessonID={lessonID}
+            setShowModal={setShowAssignmentModal}
+          />
         </Modal>
       )}
 
-        <h1>My Assignments</h1>
-          
-          <InnerContainer>
-          <Content>
+      <h1>My Assignments</h1>
 
-                    <button onClick={() => setShowAssignmentModal(true)}>
-                    Create 
-                    </button>
-                    <br />
-
+      <InnerContainer>
+        <Content>
+          <br />
+          {Assignments.length > 0 ? (
+            <div>
+              <button onClick={() => setShowAssignmentModal(true)}>
+                Create
+              </button>
               {Assignments.map((assignment) => (
-
-                  <AssignmentContainer1>
-                  <h1 style = {style} > {assignment.title}</h1>
-
-                  <ButtonContainer>
-                  <button primary = "true" onClick={() => addLessonAssignment(assignment.id)}> 
-                  Select
-                  </button>
-                  </ButtonContainer>
-
-                  <br/>
-                </AssignmentContainer1>
-              ))}
-
+                <SelectAssignmentContainer>
+                  <h1 style={style}> {assignment.title}</h1>
+                  <SelectButtonContainer>
+                    <button
+                      primary="true"
+                      onClick={() => addLessonAssignment(assignment.id)}
+                    >
+                      Select
+                    </button>
+                  </SelectButtonContainer>
                   <br />
-                  <button onClick={() => setShowModal(false)}>          
-                  Add
-                  </button>
- 
-              {Assignments.length < 1 && (
-                <AssignmentContainer>
-                <VerticalLine />
-                <FaClipboard />
-                <p> You do not have any assignments</p>
-                <button onClick={() => setShowAssignmentModal(true)}>
+                </SelectAssignmentContainer>
+              ))}
+              <AddAssignmentButton onClick={() => setShowModal(false)}>
+                Add Assignment
+              </AddAssignmentButton>
+            </div>
+          ) : (
+            <NoAssignmentsContainer>
+              <VerticalLine />
+              <FaClipboard />
+              <p> You do not have any assignments</p>
+              <button onClick={() => setShowAssignmentModal(true)}>
                 Create Assignment
-                </button>
-                </AssignmentContainer>
-              )}
-
-            </Content>
-          </InnerContainer>
+              </button>
+            </NoAssignmentsContainer>
+          )}
+        </Content>
+      </InnerContainer>
     </Fragment>
   );
 };
