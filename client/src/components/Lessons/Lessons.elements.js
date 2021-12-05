eeb8fb1df6955730fa06609423b414f59e4f374f
kaylinm <kaylin.moss1@marist.edu>
2021-12-05 14:25:14 -0500

    changed no lessons icon on lesson page

--- client/src/components/Lessons/Lessons.elements.js
+++ client/src/components/Lessons/Lessons.elements.js
@@ -4,29 +4,6 @@ import { colors } from "../../styles";
 
 const { white, primary, secondary, black } = colors;
 
-
-export const Position = styled.div`
-
-  position: absolute;
-  right: 0%;
-  left: 0%;
-  top: 40%;
- 
-
-  `;
-
-  export const ResourceContainer1 = styled.div`
-
-
-  padding: 2rem;
-  background-color: ${white};
-  border: 1px solid ${white};
-  
-  
-  
-    
-  `;
-
 export const LessonsContainer = styled.div`
   width: 100%;
   display: flex;
@@ -92,7 +69,7 @@ export const Content = styled.div`
   border-radius: 5px;
   padding: 2rem;
   background-color: ${white};
-  
+
   flex-direction: column;
   align-items: center;
   justify-content: center;
@@ -115,7 +92,6 @@ export const Content = styled.div`
     color: ${white};
     align-self: center;
     border: 1px solid ${black};
-  
   }
 `;
 
@@ -129,7 +105,6 @@ export const VerticalLine = styled.div`
 `;
 
 export const MContent = styled.div`
-
   position: absolute;
   right: 0%;
   left: 0%;
@@ -138,27 +113,21 @@ export const MContent = styled.div`
   height: 100vh;
   width: 100vw;
   z-index: 10;
-  
 `;
 
-
 const ChildContainerDropdown = styled.div`
-  
   border-radius: 5px;
   background: none;
 
   .Collapsible {
     border-radius: 5px;
     background-color: ${primary};
-    
+
     padding: 3rem;
     width: 100%;
     border: none;
-    
-   
 
     span {
-      
       flex-wrap: wrap;
       justify-content: space-between;
 
@@ -200,33 +169,21 @@ const ChildContainerDropdown = styled.div`
     background-color: ${white};
     display: flex;
     flex-direction: column;
-    
-    
-
-    
   }
 `;
 
-const ChildContainer = styled.div`
-  padding: 2rem;
-  border-radius: 25px;
-  background-color: ${white};
-
+export const Description = styled.div`
+  font-size: 1.4rem;
 `;
-
-export const Description = styled.div`font-size: 1.4rem;`;
-
 
 export const LessonsContainer1 = styled(ChildContainerDropdown)`
   grid-area: lessons;
 
   background-color: ${white};
 
-
   button {
     margin-top: 0;
     margin-right: 0;
     color: ${white};
   }
-  
 `;
--- client/src/components/Lessons/Lessons.jsx
+++ client/src/components/Lessons/Lessons.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { LessonCard } from '../index';
+import { LessonCard } from "../index";
 
 import {
   LessonsContainer,
@@ -8,99 +8,77 @@ import {
   InnerContainer,
   Content,
   LessonsContainer1,
+  VerticalLine,
 } from "./Lessons.elements";
 
 import Collapsible from "react-collapsible";
 import { FaAngleDown } from "react-icons/fa";
-import { CreateLessonForm} from "../index";
-import { FaPlus,FaClipboard } from "react-icons/fa";
-import {Modal} from "../index";
+import { CreateLessonForm } from "../index";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { Modal } from "../index";
 import { useSelector } from "react-redux";
 
 import { Fragment } from "react";
 
+function Lessons({ dashboardPaginate }) {
+  const { user } = useSelector((state) => state.userDetail);
+  const [showModal, setShowModal] = useState(false);
+  const { id } = user;
+  const { Lessons } = user;
+  const plus = { color: "white", fontSize: "2.5em" };
 
+  return (
+    <Fragment>
+      {showModal && (
+        <Modal large setShowModal={setShowModal}>
+          <CreateLessonForm creatorid={id} setShowModal={setShowModal} />
+        </Modal>
+      )}
 
-function Lessons({ dashboardPaginate}) {
+      <LessonsContainer>
+        <button onClick={() => setShowModal(true)}>
+          <span>Create Lesson</span>
+        </button>
 
-    const { user } = useSelector((state) => state.userDetail); 
-    const [showModal, setShowModal] = useState(false);
-    const { id} = user;
-    const {Lessons } = user;
-    const plus = { color: "white", fontSize: "2.5em"};
-   
-    
-
-  
-
-    return (
-        <Fragment>
-        {showModal && (
-            <Modal large setShowModal={setShowModal} >
-            <CreateLessonForm creatorid={id} setShowModal={setShowModal}/>
-            </Modal>
-        )}
-        
-        <LessonsContainer>
-        
-            <button onClick={() => setShowModal(true)}>
-                <span>Create Lesson</span> <FaPlus />
-            </button>
-        
         <TitleContainer>
-            <h1>My Lessons</h1>
-            <InnerContainer>
+          <h1>My Lessons</h1>
+          <InnerContainer>
             <Content>
-            {Lessons.map((lesson) => (
-                
+              {Lessons.map((lesson) => (
                 <LessonsContainer1>
-               
-            
-                <Collapsible
-                trigger={
-                  <React.Fragment>
-                    <h1  >{lesson.title}</h1>
-                    <FaAngleDown />
-                  </React.Fragment>
-                }
-                >
-                
-                
-                <LessonCard
-                    key={lesson.id}
-                    {...lesson}
-                    showDescription
-                    
-                />
+                  <Collapsible
+                    trigger={
+                      <React.Fragment>
+                        <h1>{lesson.title}</h1>
+                        <FaAngleDown />
+                      </React.Fragment>
+                    }
+                  >
+                    <LessonCard key={lesson.id} {...lesson} showDescription />
 
-                <br />
-                <br />
-
-                
-                </Collapsible>
-                <br></br>
-                
+                    <br />
+                    <br />
+                  </Collapsible>
+                  <br></br>
                 </LessonsContainer1>
-                
-                ))}
+              ))}
 
-                {Lessons.length < 1 && (
+              {Lessons.length < 1 && (
                 <LessonsContainer>
-                
-                <FaClipboard />
-                <p> You do not have any lessons</p>
-                <button onClick={() => setShowModal(true)}>
-                <span>Create Lesson</span> <FaPlus style={plus} />
-                </button>
+                  <VerticalLine />
+                  <FaChalkboardTeacher />
+                  <p> You do not have any lessons</p>
+                  <button onClick={() => setShowModal(true)}>
+                    <span>Create Lesson</span>
+                  </button>
                 </LessonsContainer>
-                )}
+              )}
             </Content>
-            </InnerContainer>
+          </InnerContainer>
         </TitleContainer>
-    </LessonsContainer>
+      </LessonsContainer>
     </Fragment>
-
-    );
+  );
 }
 
 export default Lessons;
