d359e4fdb53ebd8d0b4807ddcb21fd6c4cfa7e80
kaylinm <kaylin.moss1@marist.edu>
2021-12-05 14:16:36 -0500

    modified resource lesson so that add button only shows if user has a resource

--- client/src/components/ResourceLesson/ResourceLesson.elements.js
+++ client/src/components/ResourceLesson/ResourceLesson.elements.js
@@ -5,35 +5,36 @@ import { colors } from "../../styles";
 const { primary, secondary, white, black } = colors;
 
 export const ResourceContainer = styled.div`
-  
-  
-  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
+export const AddResourceButton = styled.button`
+  width: 100%;
+`;
+
+export const SelectResourceContainer = styled.div`
+  min-height: 3vh;
+  width: 100%;
+  padding: 2rem;
+  background-color: ${white};
+  border: 1px solid ${black};
   display: flex;
   flex-direction: column;
   justify-content: center;
 
-  
+  button {
+    &:hover {
+      color: ${secondary};
+      background-color: ${white};
+      border: 1px solid ${secondary};
+    }
   }
 `;
 
-
-export const ResourceContainer1 = styled.div`
-min-height: 3vh;
-width: 100%;
-padding: 2rem;
-background-color: ${white};
-border: 1px solid ${black};
-display: flex;
-flex-direction: column;
-justify-content: center;
-  
-`;
-
-
 export const InnerContainer = styled.div`
   width: 100%;
- 
   margin: 0 auto;
 `;
 
@@ -42,12 +43,11 @@ export const Content = styled.div`
   width: 100%;
   border-radius: 5px;
   padding: 2rem;
-  background-color: ${white};
+  background-color: ${primary};
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  box-shadow: inset 4px 4px 4px 2px rgba(0, 0, 0, 0.25);
 
   svg {
     font-size: 10rem;
@@ -64,28 +64,12 @@ export const Content = styled.div`
     font-size: 1.6rem;
     padding: 0.7rem 1rem;
     display: flex;
-    margin-left: 1rem;
     justify-content: space-around;
     align-items: center;
     cursor: pointer;
-    background-color: ${primary};
     border: none;
     border-radius: 5px;
-    color: ${white};
-
-    &:hover {
-        color: ${secondary};
-        background-color: ${white};
-        border: 1px solid ${secondary};
-    }
-
-    span {
-        display: inline-block;
-        margin-right: 1rem;
-        min-width: 5rem;
-    }
-}
-  
+  }
 `;
 
 export const VerticalLine = styled.div`
@@ -97,39 +81,44 @@ export const VerticalLine = styled.div`
   height: 150px;
 `;
 
+export const SelectButtonContainer = styled.div`
+  width: 35rem;
+  display: flex;
+  height: 100%;
+  justify-content: flex-end;
+  align-items: center;
 
-export const ButtonContainer = styled.div`
-    width: 35rem;
+  button {
+    background-color: ${primary};
+    color: ${white};
 
-    display: flex;
-    height: 100%;
-    justify-content: flex-end;
-    align-items: center;
-
-    button {
-        font-family: inherit;
-        font-size: 1.6rem;
-        padding: 0.7rem 1rem;
-        display: flex;
-        margin-left: 1rem;
-        justify-content: space-around;
-        align-items: center;
-        cursor: pointer;
-        background-color: ${primary};
-        border: none;
-        border-radius: 5px;
-        color: ${white};
-
-        &:hover {
-            color: ${secondary};
-            background-color: ${white};
-            border: 1px solid ${secondary};
-        }
-
-        span {
-            display: inline-block;
-            margin-right: 1rem;
-            min-width: 5rem;
-        }
+    &:hover {
+      color: ${primary};
+      background-color: ${white};
+      border: 1px solid ${primary};
     }
+  }
+`;
+
+export const NoResourcesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${primary};
+  color: rgba(255, 255, 255, 1);
+
+  svg {
+    color: rgba(255, 255, 255, 1);
+  }
+
+  button {
+    background-color: ${secondary};
+    border: 1px solid ${secondary};
+    &:hover {
+      color: ${secondary};
+      background-color: ${white};
+      border: 1px solid ${secondary};
+    }
+  }
 `;
--- client/src/components/ResourceLesson/ResourceLesson.jsx
+++ client/src/components/ResourceLesson/ResourceLesson.jsx
@@ -1,22 +1,21 @@
-import React, {useReducer, Fragment, useState, useEffect } from "react";
-import {addExistingLessonResource, editLesson} from "../../redux/actions/lessonsActions";
-import {createLessonAction} from "../../redux/actions/userActions";
+import React, { useReducer, Fragment, useState } from "react";
+import { addExistingLessonResource } from "../../redux/actions/lessonsActions";
 
 import {
-  ResourceContainer,
   InnerContainer,
   Content,
   VerticalLine,
-  ResourceContainer1,
-  ButtonContainer
+  SelectResourceContainer,
+  SelectButtonContainer,
+  NoResourcesContainer,
+  AddResourceButton,
 } from "./ResourceLesson.elements";
 
+import lessonService from "../../services/lesson.js";
 
-import lessonService from '../../services/lesson.js';
-
-import { useDispatch, useSelector } from 'react-redux';
-import { FaClipboard } from "react-icons/fa";
-import {Modal, AddResourceForm} from "../index";
+import { useDispatch, useSelector } from "react-redux";
+import { FaLayerGroup } from "react-icons/fa";
+import { Modal, AddResourceForm } from "../index";
 
 const initialState = {
   title: "",
@@ -31,105 +30,108 @@ function reducer(state, { field, value }) {
   };
 }
 
-
-const ResourceLesson = ({creatorid, lessonID, setShowModal}) => {
-
+const ResourceLesson = ({ creatorid, lessonID, setShowModal }) => {
   console.log("Resource: " + lessonID);
   console.log("Resource: " + creatorid);
 
   const { user } = useSelector((state) => state.userDetail);
-  const [ showAddModal, setShowAddModal ] = useState(false);
-  const { user: { id } } = useSelector((state) => state.userDetail);
-  const { Resources} = user;
-  const style = { color: "black"};
-  const style1 = { margin : "auto"};
-  
-  const [ state, dispatch ] = useReducer(reducer, initialState);
-	const { type, title, description, link } = state;
+  const [showAddModal, setShowAddModal] = useState(false);
+  const {
+    user: { id },
+  } = useSelector((state) => state.userDetail);
+  const { Resources } = user;
+  const style = { color: "black" };
+  const style1 = { margin: "auto" };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { type, title, description, link } = state;
 
   //redux
   const storeDispatch = useDispatch();
 
- 
-
   const addLessonResource = async (resourceID) => {
-
     const response = await lessonService.getResourceData(resourceID);
-		const { type, title, description, link } = response;
+    const { type, title, description, link } = response;
 
     let newObject = { ...response };
 
     console.log(resourceID);
     console.log(lessonID);
 
-    newObject = { ...response, mutable: true, creatorid: creatorid, LessonId: lessonID };
+    newObject = {
+      ...response,
+      mutable: true,
+      creatorid: creatorid,
+      LessonId: lessonID,
+    };
 
-    {Resources.map((resource) => !resource.LessonId && (resource.id == resourceID) &&(
-      
-      
-      resource.LessonId = lessonID,
-      storeDispatch(addExistingLessonResource(resourceID, newObject))
-
-    ))}
-
+    {
+      Resources.map(
+        (resource) =>
+          !resource.LessonId &&
+          resource.id == resourceID &&
+          ((resource.LessonId = lessonID),
+          storeDispatch(addExistingLessonResource(resourceID, newObject)))
+      );
+    }
   };
-  
 
   return (
     <Fragment>
       {showAddModal && (
         <Modal large setShowModal={setShowAddModal}>
-          <AddResourceForm lessonID={lessonID} creatorid = {creatorid} setShowModal={setShowAddModal} />
+          <AddResourceForm
+            lessonID={lessonID}
+            creatorid={creatorid}
+            setShowModal={setShowAddModal}
+          />
         </Modal>
       )}
-      
-          <h1>My Resources</h1>
-          
-          <InnerContainer>
-            <Content>
 
-                    <button onClick={() => setShowAddModal(true)}>
-                    Create 
-                    </button>
-                    <br />
+      <h1>My Resources</h1>
+
+      <InnerContainer>
+        <Content>
+          <br />
+          {Resources.length > 0 ? (
+            <div>
+              <button onClick={() => setShowAddModal(true)}>Create</button>
 
               {Resources.map((resource) => (
-                
-                    <ResourceContainer1>
-                    <h1 style = {style} >{resource.title}</h1>
-                       
-                    <ButtonContainer>
-                    <button primary = "true" onClick={() => addLessonResource(resource.id)}>
-                    Select
+                <SelectResourceContainer>
+                  <h1 style={style}>{resource.title}</h1>
+
+                  <SelectButtonContainer>
+                    <button
+                      primary="true"
+                      onClick={() => addLessonResource(resource.id)}
+                    >
+                      Select
                     </button>
-                    </ButtonContainer>
-                    <br />
-                    </ResourceContainer1>
+                  </SelectButtonContainer>
+                  <br />
+                </SelectResourceContainer>
+              ))}
 
-                ))}
-               
-                    <br />
-                    <button onClick={() => setShowModal(false)}>          
-                    Add
-                    </button>
-                    
-
-                {Resources.length < 1 && (
-                  <ResourceContainer>
-                  
-                  <FaClipboard style = {style1}/>
-                  <p> You do not have any Resources</p>
-                  <button onClick={() => setShowAddModal(true)}>
-                    Create Resource
-                  </button>
-                  </ResourceContainer>
-                )}
-                </Content>
-                </InnerContainer>
-
+              <br />
+              <AddResourceButton onClick={() => setShowModal(false)}>
+                Add Resource
+              </AddResourceButton>
+            </div>
+          ) : (
+            <NoResourcesContainer>
+              <VerticalLine />
+              <FaLayerGroup style={style1} />
+              <p> You do not have any Resources</p>
+              <button onClick={() => setShowAddModal(true)}>
+                Create Resource
+              </button>
+            </NoResourcesContainer>
+          )}
+        </Content>
+      </InnerContainer>
     </Fragment>
   );
-  
 };
 
 export default ResourceLesson;
